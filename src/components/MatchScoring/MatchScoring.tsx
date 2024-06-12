10e92badc5d2e666c5182001a07fe76c44a2dec8
MiguelCLG <MiguelCLG@hotmail.com>
2024-06-12 12:27:52 +0100

    Added cards to score list

--- src/components/MatchScoring/MatchScoring.tsx
+++ src/components/MatchScoring/MatchScoring.tsx
@@ -1,13 +1,15 @@
 import styles from "../../styles/grid-styles.module.scss";
-import { Form } from "../Form";
-import { Grid } from "../Grid";
 import { useDownloadAsCSV } from "../../hooks/useDownloadAsCSV";
 import { useNavigate, useParams } from "react-router-dom";
 import { useGetMatchById } from "../../hooks/useGetMatchById";
 import { useEffect, useState } from "react";
 import { Loader } from "../Loader/Loader";
-import { Match } from "../../utils/types";
+import { Match, MatchDataPoints } from "../../utils/types";
 import { useGetGameById } from "../../hooks/useGetGameById";
+import Card from "../Card/Card";
+import Popup from "../Popup/Popup";
+import { NewScoreForm } from "../NewGameForm/NewScoreForm";
+import { Button } from "react-bootstrap";
 
 export const MatchScoring = () => {
   const { downloadFileAsCSV } = useDownloadAsCSV();
@@ -16,6 +18,7 @@ export const MatchScoring = () => {
   const { game, fetchGame } = useGetGameById();
   const { getData, loading } = useGetMatchById();
 
+  const [showModal, setShowModal] = useState<boolean>(false);
   const [match, setMatch] = useState<Match>({} as Match);
 
   useEffect(() => {
@@ -26,12 +29,50 @@ export const MatchScoring = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleAddNewMatch = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleScoreData = (data: MatchDataPoints[]) => {
+    const result: {
+      playerName: string;
+      score: { pointsDescription: string; points: number }[];
+    }[] = [];
+
+    // Helper function to find a player in the result array
+    function findPlayer(playerName: string) {
+      return result.find(
+        (player) => player.playerName.toLowerCase() === playerName.toLowerCase()
+      );
+    }
+
+    data.forEach((item) => {
+      let player = findPlayer(item.playerName);
+      if (!player) {
+        player = {
+          playerName: item.playerName,
+          score: [],
+        };
+        result.push(player);
+      }
+      player.score.push({
+        pointsDescription: item.pointsDescription,
+        points: item.gamePoints,
+      });
+    });
+
+    return result;
+  };
+
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>
+      <h1 style={{ textAlign: "center" }} data-testid="game-title-data-test-id">
         {game !== undefined && `${game?.gameName} - Match ${matchId}`}
       </h1>
-      {loading && !match ? (
+      {loading && !match.matchDataPoints && match === ({} as Match) ? (
         <Loader />
       ) : (
         <>
@@ -43,23 +84,58 @@ export const MatchScoring = () => {
           >
             {"<"}
           </button>
-
           <div className={styles.gridContainer}>
-            {/* <GamesComboBox /> */}
-            {/* <SearchBar handleSearch={handleSearch} /> */}
-            <Grid games={match?.matchDataPoints} />
-            <Form
-              refetch={() =>
-                getData(matchId).then((response) => setMatch(response))
-              }
-            />
-            <button
-              className={styles.formButton}
-              onClick={() => downloadFileAsCSV(match?.matchDataPoints)}
+            {match?.matchDataPoints?.length > 0 &&
+              handleScoreData(match?.matchDataPoints)?.map((player, index) => (
+                <Card
+                  key={match.matchId}
+                  dataTestId={`score-card-${match.matchId}`}
+                  action={() => {}}
+                >
+                  <Card.CardTitle>{player.playerName}</Card.CardTitle>
+                  {player.score.map((score, index) => (
+                    <Card.PlayerPoints key={index}>
+                      <div>{score.pointsDescription}</div>
+                      <div>{score.points}</div>
+                    </Card.PlayerPoints>
+                  ))}
+                  <Card.DeleteButton
+                    tagKey={`delete-${match.matchId}-${index}`}
+                    action={() => {
+                      //handleDelete({ matchId: match.matchId });
+                    }}
+                  ></Card.DeleteButton>
+                </Card>
+              ))}
+            <Card
+              action={handleAddNewMatch}
+              dataTestId="add-score-card-test-id"
             >
-              Download File
-            </button>
+              <Card.CardTitle>Add Score</Card.CardTitle>
+              <Card.AddGameButton action={() => {}} />
+            </Card>
+            {showModal && (
+              <Popup handleClose={handleCloseModal}>
+                <Popup.Header>Add New Score</Popup.Header>
+                <Popup.Body>
+                  <NewScoreForm
+                    refetch={() =>
+                      getData(matchId)
+                        .then((response) => setMatch(response))
+                        .finally(() => setShowModal(false))
+                    }
+                  />
+                </Popup.Body>
+              </Popup>
+            )}
           </div>
+          <Button
+            variant="primary"
+            className={styles.formButton}
+            onClick={() => downloadFileAsCSV(match?.matchDataPoints)}
+          >
+            Download File
+          </Button>
         </>
       )}
     </>
---
+++ src/components/NewGameForm/NewScoreForm.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { MatchDataRow } from "../../utils/types";
+import { usePostMatchDataPoints } from "../../hooks/usePostMatchDataPoints";
+import { useParams } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+
+const initialFormState = {
+  playerName: "",
+  gamePoints: 0,
+  pointsDescription: "",
+};
+
+export const NewScoreForm = ({
+  refetch: handleScoreAdded,
+}: {
+  refetch: () => void;
+}) => {
+  const { error, loading, postData } = usePostMatchDataPoints();
+
+  const { matchId = 0 } = useParams();
+  const [newGameData, setNewGameData] = useState<MatchDataRow>({
+    ...initialFormState,
+  });
+
+  const handleChangeInput = (e: any) => {
+    const { name, value } = e.target;
+    const newObject = { [name]: value };
+    setNewGameData({ ...newGameData, ...newObject });
+  };
+
+  const prepareObjectForPosting = async (e: any) => {
+    e.preventDefault();
+
+    await postData(newGameData, matchId);
+
+    setNewGameData({
+      ...initialFormState,
+    });
+    handleScoreAdded();
+  };
+
+  return (
+    <Form onSubmit={prepareObjectForPosting}>
+      <Form.Group controlId="formPlayerName">
+        <Form.Label>Match Notes:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Player Name"
+          name="playerName"
+          value={newGameData.playerName}
+          onChange={handleChangeInput}
+        />
+
+        <Form.Control
+          type="number"
+          placeholder="Game Points"
+          name="gamePoints"
+          value={newGameData.gamePoints}
+          onChange={handleChangeInput}
+        />
+        <Form.Control
+          type="textarea"
+          placeholder="Points Description"
+          name="pointsDescription"
+          value={newGameData.pointsDescription}
+          onChange={handleChangeInput}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={loading}>
+        Submit
+      </Button>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+    </Form>
+  );
+};
