e92d91116a37369891e859173df9adb5044de23f
MiguelCLG <miguelclg@hotmail.com>
2026-08-09 21:38:38 +0100

    add theme to the application

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./styles/theme.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalContextProvider } from "./context/GlobalContextProvider";
